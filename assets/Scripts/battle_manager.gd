53e972926e74eaa8724af61f025c9d2a4b1154af
Gage Lappin <gagelappin119@gmail.com>
2026-02-09 21:03:51 -0500

    .

--- assets/Scripts/battle_manager.gd
+++ assets/Scripts/battle_manager.gd
@@ -1,29 +1,25 @@
 extends Node
 
-# majority of values will be replaced later on with stuff from the global script
-# hard coded for now to make it easier to test
-@export var party : Node2D
-var party_member_1 : CollisionShape2D
-var party_member_2 : CollisionShape2D
-var party_member_3 : CollisionShape2D
+# Global Variables / signals
+# --------------------------------------------------------------------------------
+
+@export var party_members : Array[PartyMember]
+var party_nodes : Array[CollisionShape2D]
+@export var party_labels : Array[VBoxContainer]
 
 var number = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-var is_dragging : bool = false
-var current_hovered_enemy : Node2D = null
-var selected_target: Node2D = null
+
+var battle_state = {
+	"is_dragging": false,
+	"selected_target": null,
+	"hovered_enemy": null,
+	"party_has_won": false,
+	"turn_order": []
+}
+
 var party_has_won: bool = false
 
 @export var current_encounter : encounters
-
-# Will be set up by global later on
-@export var party_1 : PartyMember
-@export var party_2 : PartyMember
-@export var party_3 : PartyMember
-
-@export var party_1_label : VBoxContainer
-@export var party_2_label : VBoxContainer
-@export var party_3_label : VBoxContainer
-
 @export var enemy_list : Array[Node2D]
 @export var enemy_enclosure : Node2D
 
@@ -33,58 +29,93 @@ var active_enemies_data : Array = []
 
 signal action_taken
 
-func _update_ui(current_health: int, party_member: int):
-	match party_member:
-		0:
-			party_1_label.get_node("HBoxContainer").get_child(1).text = str(current_health) + " / " + str(party_1.player_stats.max_health)
-			if party_1.player_stats.health == 0:
-				party_member_1.get_node("X").visible = true
-			else:
-				party_member_1.get_node("X").visible = false
-		1:
-			party_2_label.get_node("HBoxContainer").get_child(1).text = str(current_health) + " / " + str(party_2.player_stats.max_health)
-			if party_2.player_stats.health == 0:
-				party_member_2.get_node("X").visible = true
-			else:
-				party_member_2.get_node("X").visible = false
-		2:
-			party_3_label.get_node("HBoxContainer").get_child(1).text = str(current_health) + " / " + str(party_3.player_stats.max_health)
-			if party_3.player_stats.health == 0:
-				party_member_3.get_node("X").visible = true
-			else:
-				party_member_3.get_node("X").visible = false
+# Helper Functions / Classes
+# --------------------------------------------------------------------------------
 
-# Given current encounter / party arrangement assign the appropriate sprites and hide the color_rect on the combat_templates
+# class to store party / enemies in relation to their speed
+class TurnStorage:
+	var combatant
+	var speed : int
+	func _init(_combatant, _speed: int):
+		combatant = _combatant
+		speed = _speed
+
+# Returns an array of active combatants sorted by speed in descending order
+func turn_priority(array : Array[TurnStorage]):
+	array.sort_custom(func(a, b):
+		return a.speed > b.speed
+	)
+
+func is_combatant_alive(combatant) -> bool:
+	for order in turn_orders:
+		if order.combatant == combatant:
+			return true
+	return false
+
+func check_if_enemy_is_dead(target_node, target_data):
+	if target_data.enemy_stats.health <= 0: 
+		print("Enemy has died: " + target_data.enemy_name)
+		target_node.visible = false
+		target_node.get_node("Area2D/CollisionShape2D").set_deferred("disabled", true)
+		remove_combatant_from_turns(target_data)
+	
+func remove_combatant_from_turns(combatant_to_remove):
+	for i in range(turn_orders.size() - 1, -1, -1):
+		if turn_orders[i].combatant == combatant_to_remove:
+			turn_orders.remove_at(i)
+
+func determine_can_move(entity):
+	var thing = active_turn_orders.get(0).combatant
+	var not_enemy_turn : bool = thing is PartyMember
+	var can_move : bool
+	if not_enemy_turn == true:
+		if thing.current_battle_position == entity.current_battle_position:
+			can_move = true
+		else:
+			can_move = false
+	return can_move
+
+func _on_button_button_down():
+	party_members[0].take_damage(50)
+	party_members[1].take_damage(50)
+	party_members[2].take_damage(50)
+
+func _on_button_2_button_down():
+	party_members[0].heal_member(50)
+	party_members[1].heal_member(50)
+	party_members[2].heal_member(50)
+
+func _update_ui(current_health: int, cur_player: int):
+	party_labels[cur_player].get_node("HBoxContainer").get_child(1).text = str(current_health) + " / " + str(party_members[cur_player].player_stats.max_health)
+	if party_members[cur_player].player_stats.health == 0:
+		party_nodes[cur_player].get_node("X").visible = true
+	else:
+		party_nodes[cur_player].get_node("X").visible = false
+
+# Main Battle Loop
+# --------------------------------------------------------------------------------
+
 func _ready():
 	number.shuffle()
 	
-	party_member_1 = party.get_node("Front")
-	party_member_2 = party.get_node("Middle")
-	party_member_3 = party.get_node("Back")
+	var party = $CombatParty
 	
-	party_1_label.get_child(0).text = party_1.member_name
-	party_2_label.get_child(0).text = party_2.member_name
-	party_3_label.get_child(0).text = party_3.member_name
+	party_nodes.append(party.get_node("Front"))
+	party_nodes.append(party.get_node("Middle"))
+	party_nodes.append(party.get_node("Back"))
+	
+	for i in range(party_members.size()):
+		party_labels[i].get_child(0).text = party_members[i].member_name
+		party_members[i].player_stats.health_changed.connect(_update_ui.bind(i))
+		party_members[i].player_stats.speed_changed.connect(turn_priority.bind(turn_orders))
+		_update_ui(party_members[i].player_stats.health, i)
+		party_members[i].current_battle_position = i
+		turn_orders.append(TurnStorage.new(party_members[i], (party_members[i].player_stats.altered_speed + party_members[i].player_stats.speed)))
+		party_nodes[i].get_node("Mouse_Spot").input_event.connect(_on_party_input_event.bind(i))
 
-	party_1.player_stats.health_changed.connect(_update_ui.bind(0))
-	party_2.player_stats.health_changed.connect(_update_ui.bind(1))
-	party_3.player_stats.health_changed.connect(_update_ui.bind(2))
-	
-	party_1.player_stats.speed_changed.connect(turn_priority.bind(turn_orders))
-	party_2.player_stats.speed_changed.connect(turn_priority.bind(turn_orders))
-	party_3.player_stats.speed_changed.connect(turn_priority.bind(turn_orders))
-
-	_update_ui(party_1.player_stats.health, 0)
-	_update_ui(party_2.player_stats.health, 1)
-	_update_ui(party_3.player_stats.health, 2)
-	
-	party_1.current_battle_position = 0
-	party_2.current_battle_position = 1
-	party_3.current_battle_position = 2
-	
-	party.get_node("Front").get_node("Party_Member").texture = party_1.player_sprite
-	party.get_node("Middle").get_node("Party_Member").texture = party_2.player_sprite
-	party.get_node("Back").get_node("Party_Member").texture = party_3.player_sprite
+	party.get_node("Front").get_node("Party_Member").texture = party_members[0].player_sprite
+	party.get_node("Middle").get_node("Party_Member").texture = party_members[1].player_sprite
+	party.get_node("Back").get_node("Party_Member").texture = party_members[2].player_sprite
 	
 	active_enemies_data.clear()
 	
@@ -104,10 +135,6 @@ func _ready():
 			
 			visual_node.set_meta("data_index", i)
 		
-	turn_orders.append(TurnStorage.new(party_1, (party_1.player_stats.altered_speed + party_1.player_stats.speed)))
-	turn_orders.append(TurnStorage.new(party_2, (party_2.player_stats.speed + party_2.player_stats.altered_speed)))
-	turn_orders.append(TurnStorage.new(party_3, (party_3.player_stats.altered_speed + party_3.player_stats.speed)))
-	
 	for enemy in active_enemies_data:
 		turn_orders.append(TurnStorage.new(enemy, (enemy.enemy_stats.speed + enemy.enemy_stats.altered_speed)))
 	
@@ -122,22 +149,10 @@ func _ready():
 	
 	take_turn()
 
-class TurnStorage:
-	var combatant
-	var speed : int
-	func _init(_combatant, _speed: int):
-		combatant = _combatant
-		speed = _speed
-
-func turn_priority(array):
-	array.sort_custom(func(a, b):
-		return a.speed > b.speed
-	)
-
 # executes turns in order of speed
 func take_turn():
-	while not party_has_won:
-		if party_1.player_stats.health <= 0 and party_2.player_stats.health <= 0 and party_3.player_stats.health <= 0:
+	while not battle_state["party_has_won"]:
+		if party_members[0].player_stats.health <= 0 and party_members[1].player_stats.health <= 0 and party_members[2].player_stats.health <= 0:
 			get_tree().quit()
 			return 
 		
@@ -163,72 +178,21 @@ func take_turn():
 			if active_turn_orders.size() > 0:
 				active_turn_orders.remove_at(0)
 
-func is_combatant_alive(combatant) -> bool:
-	for order in turn_orders:
-		if order.combatant == combatant:
-			return true
-	return false
-
 func player_turn(cur_player: int):
-	match cur_player:
-		0:
-			print("Party 1 Doing stuff")
-			if party_1.is_dead:
-				return
-			party_member_1.get_node("Selection_Arrow").visible = true
-			await action_taken
-			party_member_1.get_node("Selection_Arrow").visible = false
-			party_member_1.get_node("Line").visible = false
-			if selected_target.has_meta("data_index"):
-				var data_idx = selected_target.get_meta("data_index")
-				var target_data = active_enemies_data[data_idx]
-				
-				var damage = party_1.calculate_damage()
-				target_data.take_damage(damage)
-				check_if_enemy_is_dead(selected_target, target_data)
-		1:
-			print("Party 2 doing stuff")
-			if party_2.is_dead:
-				return
-			party_member_2.get_node("Selection_Arrow").visible = true
-			await action_taken
-			party_member_2.get_node("Selection_Arrow").visible = false
-			party_member_2.get_node("Line").visible = false
-			if selected_target.has_meta("data_index"):
-				var data_idx = selected_target.get_meta("data_index")
-				var target_data = active_enemies_data[data_idx]
-				
-				var damage = party_2.calculate_damage()
-				target_data.take_damage(damage)
-				check_if_enemy_is_dead(selected_target, target_data)
-		2:
-			print("Party 3 doing stuff")
-			if party_3.is_dead:
-				return
-			party_member_3.get_node("Selection_Arrow").visible = true
-			await action_taken
-			party_member_3.get_node("Selection_Arrow").visible = false
-			party_member_3.get_node("Line").visible = false
-			if selected_target.has_meta("data_index"):
-				var data_idx = selected_target.get_meta("data_index")
-				var target_data = active_enemies_data[data_idx]
-				
-				var damage = party_3.calculate_damage()
-				target_data.take_damage(damage)
-				check_if_enemy_is_dead(selected_target, target_data)
-	selected_target = null
-
-func check_if_enemy_is_dead(target_node, target_data):
-	if target_data.enemy_stats.health <= 0: 
-		print("Enemy has died: " + target_data.enemy_name)
-		target_node.visible = false
-		target_node.get_node("Area2D/CollisionShape2D").set_deferred("disabled", true)
-		remove_combatant_from_turns(target_data)
-	
-func remove_combatant_from_turns(combatant_to_remove):
-	for i in range(turn_orders.size() - 1, -1, -1):
-		if turn_orders[i].combatant == combatant_to_remove:
-			turn_orders.remove_at(i)
+	if party_members[cur_player].is_dead:
+		return
+	party_nodes[cur_player].get_node("Selection_Arrow").visible = true
+	await action_taken
+	party_nodes[cur_player].get_node("Selection_Arrow").visible = false
+	party_nodes[cur_player].get_node("Line").visible = false
+	if battle_state["selected_target"].has_meta("data_index"):
+		var data_idx = battle_state["selected_target"].get_meta("data_index")
+		var target_data = active_enemies_data[data_idx]
+		
+		var damage = party_members[cur_player].calculate_damage()
+		target_data.take_damage(damage)
+		check_if_enemy_is_dead(battle_state["selected_target"], target_data)
+	battle_state["selected_target"] = null
 	
 func enemy_turn(cur_enemy: int):
 	var enemy_data = current_encounter.enemy_list.get(cur_enemy)
@@ -236,82 +200,42 @@ func enemy_turn(cur_enemy: int):
 		return
 	var enemy_damage = enemy_data.calculate_damage()
 	var random_party_member = randi_range(0, 2)
-	match random_party_member:
-		0:
-			party_1.take_damage(enemy_damage)
-		1:
-			party_2.take_damage(enemy_damage)
-		2:
-			party_3.take_damage(enemy_damage)
-			
-func _on_button_button_down():
-	party_1.take_damage(50)
-	party_2.take_damage(50)
-	party_3.take_damage(50)
+	party_members[random_party_member].take_damage(enemy_damage)
 
-func _on_button_2_button_down():
-	party_1.heal_member(50)
-	party_2.heal_member(50)
-	party_3.heal_member(50)
+# Stuff to handle actions
+# --------------------------------------------------------------------------------
+
+func _on_party_input_event(viewport, event, shape_idx, cur_party: int):
+	var can_move = determine_can_move(party_members[cur_party])
+	
+	if event is InputEventMouseButton and can_move:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				party_nodes[cur_party].get_node("Line").visible = true
+				battle_state["is_dragging"] = true
+
+func mouse_released():
+	if battle_state["is_dragging"]:
+		battle_state["is_dragging"] = false
+		for node in party_nodes:
+			node.get_node("Line").visible = false
 
 func _unhandled_input(event):
 	if event is InputEventMouseButton and not event.pressed:
-		if is_dragging:
-			if current_hovered_enemy != null:
-				lock_selection(current_hovered_enemy)
+		if battle_state["is_dragging"]:
+			if battle_state["hovered_enemy"] != null:
+				lock_selection(battle_state["hovered_enemy"])
 			else:
 				mouse_released()
 
 func lock_selection(enemy):
-	is_dragging = false
-	selected_target = enemy
+	battle_state["is_dragging"] = false
+	battle_state["selected_target"] = enemy
 	action_taken.emit()
 
-func mouse_released():
-	if is_dragging:
-		is_dragging = false
-		party_member_3.get_node("Line").visible = false
-		party_member_2.get_node("Line").visible = false
-		party_member_1.get_node("Line").visible = false
-
-func determine_can_move(entity):
-	var thing = active_turn_orders.get(0).combatant
-	var not_enemy_turn : bool = thing is PartyMember
-	var can_move : bool
-	if not_enemy_turn == true:
-		if thing.current_battle_position == entity.current_battle_position:
-			can_move = true
-		else:
-			can_move = false
-	return can_move
-
-func _on_back_party_entered(viewport, event, shape_idx):
-	var can_move = determine_can_move(party_3)
-	if event is InputEventMouseButton and can_move:
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			if event.pressed:
-				party_member_3.get_node("Line").visible = true
-				is_dragging = true
-				
-func _on_middle_party_entered(viewport, event, shape_idx):
-	var can_move = determine_can_move(party_2)
-	if event is InputEventMouseButton and can_move:
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			if event.pressed:
-				party_member_2.get_node("Line").visible = true
-				is_dragging = true
-
-func _on_front_party_entered(viewport, event, shape_idx):
-	var can_move = determine_can_move(party_1)
-	if event is InputEventMouseButton and can_move:
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			if event.pressed:
-				party_member_1.get_node("Line").visible = true
-				is_dragging = true
-
 func _on_any_enemy_entered(enemy_node):
-	current_hovered_enemy = enemy_node
+	battle_state["hovered_enemy"] = enemy_node
 
 func _on_any_enemy_exited(enemy_node):
-	if current_hovered_enemy == enemy_node:
-		current_hovered_enemy = null
+	if battle_state["hovered_enemy"] == enemy_node:
+		battle_state["hovered_enemy"] = null
--- assets/Scripts/lines.gd
+++ assets/Scripts/lines.gd
@@ -1,6 +1,5 @@
 extends Line2D
 
-# We create the Curve2D resource in memory
 var curve = Curve2D.new()
 
 
@@ -8,18 +7,13 @@ func _process(delta):
 	var start_pos = Vector2.ZERO
 	var target_pos = get_local_mouse_position()
 	
-	# 1. Get the angle from the card to the mouse
 	var mouse_angle = target_pos.angle() 
 	
 	var distance = start_pos.distance_to(target_pos)
 	var control_strength = distance * 0.5
 	
-	# 2. To create a curve, the start tangent shouldn't point DIRECTLY at the mouse.
-	# If it points directly at the mouse, the line will be straight.
-	# We rotate it slightly (e.g., -45 degrees) so it "swings" out before heading to the target.
 	var start_out = Vector2.from_angle(mouse_angle - deg_to_rad(45)) * control_strength
 	
-	# 3. The end tangent comes "in" from the opposite side to meet the mouse
 	var end_in = Vector2.from_angle(mouse_angle + deg_to_rad(180)) * control_strength
 
 	curve.clear_points()
